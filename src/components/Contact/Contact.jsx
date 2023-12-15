ecf5af0783c205ddc7359b9a68e9f3fec53c961b
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-12-15 11:10:24 +0600

    Update contactInfo section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,28 +8,30 @@ const Contact = () => {
     <>
     <SectionTitle Title="<Contact Information" />
   
-    <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-5 p-6 bg-artificialBlue rounded-md shadow-md">
+    <div className="my-10 grid grid-cols-1 text-white md:grid-cols-2 lg:grid-cols-3 items-center gap-5 p-6 bg-artificialBlue rounded-md shadow-md">
       <div className="flex flex-col items-center space-x-4">
-        <div className="text-black flex text-3xl items-center gap-1">
+        <div className="text-white flex text-3xl items-center gap-1">
           <span className=""><FaSearchLocation /></span> <span className="font-semibold">Location</span>
         </div>
         <p className="">Jaintapur, Sylhet, Bangladesh</p>
       </div>
       <div className="flex flex-col items-center space-x-4">
-        <div className="text-black flex text-3xl items-center gap-3">
+        <div className="text-white flex text-3xl items-center gap-3">
           <span className=""><FaPhoneAlt /></span> <span className="font-semibold">Phone</span>
         </div>
         <p className="">+8801616155648</p>
       </div>
       <div className="flex flex-col items-center space-x-4">
-        <div className="text-black flex text-3xl items-center gap-3">
+        <div className="text-white flex text-3xl items-center gap-3">
           <span className=""><MdEmail /></span> <span className="font-semibold">Email</span>
         </div>
         <p className="">Jahidsarkar2121@gmail.com</p>
       </div>
     </div>
   
+    <div className="flex justify-end">
     <SectionTitle Title="/>" />
+    </div>
   </>
   );
 };
