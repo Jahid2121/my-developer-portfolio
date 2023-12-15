56ccbeb54f76fd0545c60ce57c5758a33d5793ba
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-12-15 10:11:07 +0600

    Add Open live link by clicking project

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -8,9 +8,11 @@ const Project = ({ project }) => {
   return (
     <div className="container bg-artificialPastel">
       <Tooltip id="clickEvent" />
-      <div data-tooltip-id="clickEvent" data-tooltip-content="Click to Open Live Link"  style={{backgroundImage: `url(${project.img})`}} className="box box-1 bg-cover h-[300px] cursor-pointer transition-all group-hover:bg-bottom ease-in-out duration-[6s]">
+      <Link to={project.liveLink}>
+      <div data-tooltip-id="clickEvent" data-tooltip-content="Click to Open Live Preview"  style={{backgroundImage: `url(${project.img})`}} className="box box-1 bg-cover h-[300px] cursor-pointer transition-all group-hover:bg-bottom ease-in-out duration-[6s]">
 
 </div>
+      </Link>
 <div className="card-body">
         <h2 className="card-title">{project.projectName}</h2>
         <p>{project.details}</p>
